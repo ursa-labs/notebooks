613255b792a97b6dbcb380d4f8b7ea1a697f7345
Wes McKinney <wesm+git@apache.org>
2020-04-15 18:06:15 -0500

    Update make_plots.R

--- 20190919file_benchmarks/make_plots.R
+++ 20190919file_benchmarks/make_plots.R
@@ -1,31 +1,27 @@
 library(ggplot2)
 
-setwd("~/code/notebooks/20190919file_benchmarks/")
+# setwd("~/code/notebooks/20190919file_benchmarks/")
 
-results <- read.csv("all_results.csv")
+read_results <- read.csv("all_read_results.csv")
+write_results <- read.csv("all_write_results.csv")
 file_sizes <- read.csv("file_sizes.csv")
 
-ggplot(results, aes(fill=factor(nthreads), y=time, x=expr)) +
+ggplot(read_results, aes(fill=factor(nthreads), y=time, x=expr)) +
   facet_grid(rows=vars(output_type), col=vars(dataset)) +
   geom_bar(position="dodge", stat="identity") +
   coord_flip()
 
-ggplot(results[results$language == "R",], aes(fill=factor(nthreads), y=time, x=expr)) +
-  facet_grid(rows=vars(dataset)) +
+ggsave("20200414_read_results.png", width=10, height=6)
+
+ggplot(write_results, aes(fill=factor(nthreads), y=time, x=expr)) +
+  facet_grid(rows=vars(output_type), col=vars(dataset)) +
   geom_bar(position="dodge", stat="identity") +
   coord_flip()
 
-ggplot(results[results$language == "Python",], aes(fill=factor(nthreads), y=time, x=expr)) +
-  facet_grid(cols=vars(dataset), rows=vars(output_type)) +
-  geom_bar(position="dodge", stat="identity") +
-  coord_flip()
-
-results
-
-ggsave("results_20200412.png", width=10, height=6)
+ggsave("20200414_write_results.png", width=10, height=6)
 
 ggplot(file_sizes, aes(fill=file_type, y=size, x=dataset)) +
   geom_bar(position="dodge", stat="identity") +
   coord_flip()
 
-ggsave("file_sizes_20200412.png", width=10, height=3)
+ggsave("20200414_file_sizes.png", width=10, height=3)
